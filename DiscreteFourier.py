35035314049d4a3c72b2eb3f80d0e4062d9ab09e
simo4-4 <59541717+simo4-4@users.noreply.github.com>
2023-03-30 13:55:53 -0400

    update

--- DiscreteFourier.py
+++ DiscreteFourier.py
@@ -1,18 +1,36 @@
 import numpy as np
 
 
-class DiscreteFourier:
+def naive_transform(signal):
+    N = len(signal)
+    fourier = np.zeros(N, dtype=np.complex_)
 
-    def __init__(self, signal):
-        self.signal = signal
-        self.N = len(signal)
-        self.fourier = np.zeros(self.N, dtype=np.complex_)  # initialize array of 0 with complex type
-
-    def naive_transform(self, signal, N):
-        for k in range(N):
-            for n in range(N):
-                self.fourier[k] += signal[n] * np.exp(((-2j * np.pi) / N) * (k * n))
-        return self.fourier
+    for k in range(N):
+        for n in range(N):
+            fourier[k] += signal[n] * np.exp((-2j * np.pi * k * n) / N)
+    return fourier
 
 
+def FFT(signal):
+    N = len(signal)
 
+    if N <= 4:
+        return naive_transform(signal)
+    else:
+        X_even = FFT(signal[::2])
+        X_odd = FFT(signal[1::2])
+        factor = np.exp(-2j * np.pi * np.arange(N) / N)
+
+        X = np.concatenate([X_even + factor[:int(N / 2)] * X_odd, X_even + factor[int(N / 2):] * X_odd])
+        return X
+
+
+#[[1,2,3],[3,4,5],[3,6,3]]
+def FFT_2D(signal):
+    N = len(signal)
+
+    if N == 1:
+        return FFT(signal[0])
+    else:
+        factor = np.exp()
+        return X
--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-import DiscreteFourier as df
-from PIL import Image
+import DiscreteFourier as Df
+import time
 import numpy as np
+import math
+
+
+# function to compare values in an array and print values that are not equal to each other
+def compare_arrays(array1, array2):
+    for i in range(len(array1)):
+        if math.floor(array1[i]) != math.floor(array2[i]):
+            print(f"Index: {i} \t Array1: {array1[i]} \t Array2: {array2[i]}")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('Discrete Fourier Transform\n\n')
-    signal = [1, 0, -1, 0]
-    signal = [0, 1, 0, -1]
-    signal = [-1,2,3,0]
-    N = len(signal)
-    fourier = df.DiscreteFourier(signal)
-    dft = fourier.naive_transform(signal, N)
-    print(dft)
+    signal = [3, 9, 11, 13, 15, 17]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    signal = np.random.randint(0, 10, 1024)
+
+    tic = time.perf_counter()
+    naive = Df.naive_transform(signal)
+    toc = time.perf_counter()
+    print(f"Naive in  {toc - tic:0.4f} seconds")
+
+
+    tic = time.perf_counter()
+    berkeley = Df.FFT(signal)
+    toc = time.perf_counter()
+    print(f"Berkeley in  {toc - tic:0.4f} seconds \n\n")
+
